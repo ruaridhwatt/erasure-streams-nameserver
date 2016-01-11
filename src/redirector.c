8507c181c258f3bb3d17ee88d1b64d54fa4cf9b2
Ruaridh Watt <ruaridh.watt@gmail.com>
2016-01-11 12:16:29 +0100

    free sent

--- src/redirector.c
+++ src/redirector.c
@@ -12,20 +12,23 @@
 int callback_redirector(struct libwebsocket_context *ctx, struct libwebsocket *wsi,
 		enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len) {
 
+	int res;
 	char *redirect;
 
+	res = 0;
 	switch (reason) {
 	case LWS_CALLBACK_ESTABLISHED:
 		redirect = getRedirect();
 		if (redirect == NULL) {
 			break;
 		}
-		libwebsocket_write(wsi, (unsigned char *) &redirect[LWS_SEND_BUFFER_PRE_PADDING],
+		res = libwebsocket_write(wsi, (unsigned char *) &redirect[LWS_SEND_BUFFER_PRE_PADDING],
 				strlen(&redirect[LWS_SEND_BUFFER_PRE_PADDING]), LWS_WRITE_TEXT);
+		free(redirect);
 		break;
 	default:
 		break;
 	}
-	return 0;
+	return res;
 }
 
